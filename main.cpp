4c404f547b95e73f14db68744e85ff4d7467b0ea
Lciwxzr <981421391@qq.com>
2019-09-23 20:12:14 +0800

    Add some basement test and do some simple io

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "gromacs/fileio/xtcio.h"
 
 using namespace std;
 
 int main()
 {
+    char f_name[1000];
+//    cin >> f_name;
+    cin.getline(f_name, 6);
+    t_fileio* xtc_test;
+    xtc_test = open_xtc(f_name, "r");
     cout << "Hello World!" << endl;
     return 0;
 }
