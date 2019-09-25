a2f8cf9097fc74319e7586d6dbd368e2d3ad32fd
Lciwxzr <981421391@qq.com>
2019-09-25 17:31:31 +0800

    Add an container containing all the coordinates of atoms (without allocate memory in advance)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 #include "gromacs/fileio/xtcio.h"
+#include "stdlib.h"
+#include <vector>
 
-using namespace std;
 
 int main()
 {
-    char f_name[1000];
-//    cin >> f_name;
-    cin.getline(f_name, 6);
-    t_fileio* xtc_test;
-    xtc_test = open_xtc(f_name, "r");
-    cout << "Hello World!" << endl;
+
+    t_fileio   *xd;
+    int         indent;
+    char        buf[256];
+    rvec       *x;
+    matrix      box;
+    int         nframe, natoms;
+    int64_t     step;
+    real        prec, time;
+    gmx_bool    bOK;
+    int criter;
+    rvec * cor;
+    std::vector<rvec*> cor_set;
+
+
+
+    char fn[1000] ;
+    std::cout<<"input the path of file :";
+    std::cin >> fn;
+//    cin.getline(fn, 6);
+    xd = open_xtc(fn, "r");
+    read_first_xtc(xd, &natoms, &step, &time, box, &x, &prec, &bOK);
+    for (int iteror=0;iteror<natoms;iteror++)
+    {
+        cor_set.push_back(x+iteror);
+    }
+
+    criter = read_next_xtc(xd, natoms, &step, &time, box, x, &prec, &bOK) ;
+
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
