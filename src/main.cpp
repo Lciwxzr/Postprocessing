6074b5f001e9851d768c42f550d90c6f3072f1dd
Lciwxzr <981421391@qq.com>
2019-10-01 09:54:56 +0800

    Add code for reading index file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "stdlib.h"
 #include <vector>
 #include "cal_func.h"
+#include "gromacs/topology/index.h"
 
 
 
@@ -18,6 +19,7 @@ int main()
     gmx_bool    bOK;
     int criter;
     std::vector<rvec*> coor_set;
+    std::vector<std::vector<rvec*>> coor_set_time;
     std::vector<float> result;
 //    you need to edit the type of the two result cache variable in order to fit your own needs
     float res_temp, res_cache;
@@ -26,22 +28,61 @@ int main()
 
 
     char fn[1000] ;
-    std::cout<<"input the path of file :";
+    std::cout<<"input the path of file :\n";
     std::cin >> fn;
 //    cin.getline(fn, 6);
     xd = open_xtc(fn, "r");
     read_first_xtc(xd, &natoms, &step, &time, box, &x, &prec, &bOK);
-    coor_set.reserve(natoms);
-    for (int iteror=0;iteror<natoms;iteror++)
-    {
-        coor_set.push_back(x+iteror);
-    }
+
     do{
+        for (int iteror=0;iteror<natoms;iteror++)
+        {
+            coor_set.push_back(x+iteror);
+        }
+        coor_set_time.push_back(coor_set);
         res_cache = count_value(coor_set, res_temp);
+
         result.push_back(res_cache);
+        for (int iteror=0;iteror<natoms;iteror++)
+        {
+            coor_set.pop_back();
+        }
+
+        x = new rvec[natoms];
         criter = read_next_xtc(xd, natoms, &step, &time, box, x, &prec, &bOK) ;
     }while(criter!=0);
-    std::cout << "Hello World!" << std::endl;
+
+//    code above is for frame reading and getting coordinate info of single atom
+
+//    code below is for reading index
+
+
+    int *index, isize;
+    char n_file[1000];
+    char *grpname;
+
+
+    std::cout<<"input the ndx file to read:\n";
+    std::cin>>n_file;
+
+
+
+
+    rd_index(n_file, 1, &isize, &index, &grpname);
+
+    std::vector<int> group_index_vec;
+
+
+    for(int iteror=0; iteror<isize; iteror++)
+    {
+        group_index_vec.push_back(*(index+iteror));
+
+    }
+
+//    these code can generate a set of index which give a reference to atoms for analysis
+
+    std::cout<<"debug stoppoint";
+
     return 0;
 }
 
